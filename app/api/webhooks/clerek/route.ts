4cf286f87d25ec7dde49cbdf2a9bd6720c2b09fb
ayoub <ayoub@tekos.co>
2024-02-28 11:28:19 +0100

    fix clerk webhooks 2

--- app/api/webhooks/clerek/route.ts
+++ app/api/webhooks/clerek/route.ts
@@ -1,21 +1,13 @@
-import { Webhook } from "svix";
+/* eslint-disable camelcase */
+import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
-import { buffer } from "micro";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405);
-  }
+export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
@@ -26,18 +18,21 @@ export default async function handler(
   }
 
   // Get the headers
-  const svix_id = req.headers["svix-id"] as string;
-  const svix_timestamp = req.headers["svix-timestamp"] as string;
-  const svix_signature = req.headers["svix-signature"] as string;
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return res.status(400).json({ error: "Error occured -- no svix headers" });
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
 
-  console.log("headers", req.headers, svix_id, svix_signature, svix_timestamp);
   // Get the body
-  const body = (await buffer(req)).toString();
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
@@ -53,17 +48,70 @@ export default async function handler(
     }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
-    return res.status(400).json({ Error: err });
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
 
+  // CREATE
+  if (eventType === "user.created") {
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
+
+    const newUser = await createUser(user);
+
+    // Set public metadata
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: "OK", user: newUser });
+  }
+
+  // UPDATE
+  if (eventType === "user.updated") {
+    const { id, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
+    };
+
+    const updatedUser = await updateUser(id, user);
+
+    return NextResponse.json({ message: "OK", user: updatedUser });
+  }
+
+  // DELETE
+  if (eventType === "user.deleted") {
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser(id!);
+
+    return NextResponse.json({ message: "OK", user: deletedUser });
+  }
+
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
-  if (eventType === "user.created") {
-    // here create user in DB
-  }
-  return res.status(200).json({ response: "Success" });
+
+  return new Response("", { status: 200 });
 }
